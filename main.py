0121dfe083c4eabe96775a8bbb78d084c946de38
lirena <sakshamkushwaha12776@gmail.com>
2024-08-19 22:01:53 +0530

    fix : file exceptions

--- main.py
+++ main.py
@@ -11,19 +11,22 @@ async def home():
 
 @app.get("/random")
 async def random_expansion(abbr=None):
-    if abbr is not None:
-        selected_file = f'{abbr.lower()}.txt'
-        filepath = f'expansions\{selected_file}'
-    else:
-        files = os.listdir('expansions')
-        selected_file = random.choice(files)
-        filepath = os.path.join('expansions', selected_file)
+    try:
+        if abbr is not None:
+            selected_file = f'{abbr.lower()}.txt'
+            filepath = f'expansions\{selected_file}'
+        else:
+            files = os.listdir('expansions')
+            selected_file = random.choice(files)
+            filepath = os.path.join('expansions', selected_file)
 
-    with open(filepath, 'r') as file:
-        lines = file.readlines()
-    
-    selected_line = random.choice(lines)
+        with open(filepath, 'r') as file:
+            lines = file.readlines()
+        
+        selected_line = random.choice(lines)
 
-    return {
-        "abbreviation": selected_file.replace('.txt', '').upper(),
-        "expansion": selected_line.strip()}
+        return {
+            "abbreviation": selected_file.replace('.txt', '').upper(),
+            "expansion": selected_line.strip()}
+    except Exception as e:
+        return {"error": str(e)}
